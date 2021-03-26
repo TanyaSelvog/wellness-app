7d1f0c1de43023ca5a219d23d812c9663ea8c9e1
Tanya Selvog <tanyaselvog@gmail.com>
2021-03-26 12:59:09 -0500

    added love-flower favicon

--- src/components/areas.js
+++ src/components/areas.js
@@ -4,11 +4,13 @@ export default class areas extends Component {
     render() {
         return (
           <div className="dimensions">
-          <h2 id="areas">Areas</h2>
+          <h2 id="areas">Areas of Wellness</h2>
           <ul>
             <li>Financial</li>
             <li>Physical</li>
             <li>Social</li>
+            <li>Intellectual</li>
+            <li>Occupational</li>
             <li>Spiritual</li>
             <li>Emotional</li>
             <li>Environmetal</li>

b65c389761ffa2da4a38038fefb25ecf9b76bbb1
Tanya Selvog <tanyaselvog@gmail.com>
2021-03-27 15:39:20 -0500

    edited CSS in areas section

--- src/components/areas.js
+++ src/components/areas.js
@@ -3,20 +3,23 @@ import React, { Component } from 'react'
 export default class areas extends Component {
     render() {
         return (
-          <div className="dimensions">
-          <h2 id="areas" className="Box-title d-flex flex-justify-center">Areas of Wellness</h2>
+          <div className="areas"> 
+           <h2>Areas of Wellness</h2>
+           <div className="wellness">
+              <ul>
+              <li>Financial</li>
+              <li>Physical</li>
+              <li>Social</li>
+              <li>Intellectual</li>
+              <li>Occupational</li>
+              <li>Spiritual</li>
+              <li>Emotional</li>
+              <li>Environmetal</li>
+              </ul>
+              </div>
           
-          <ul>
-            <li>Financial</li>
-            <li>Physical</li>
-            <li>Social</li>
-            <li>Intellectual</li>
-            <li>Occupational</li>
-            <li>Spiritual</li>
-            <li>Emotional</li>
-            <li>Environmetal</li>
-          </ul>
           </div>
+          
           
             
         )
